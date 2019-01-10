c6ddc3c0449408b45bdb6721d8f26839c2c422b7
Geoff Doig <geoff.doig71@gmail.com>
2019-01-10 16:09:46 +0000

    Setup basic html pages and routing

--- run.py
+++ run.py
@@ -1,11 +1,19 @@
 import os
-from flask import Flask
+from flask import Flask, render_template
 
 app = Flask(__name__)
 
 @app.route("/")
 def index():
-    return "<h1>Welcome to Game</h1>"
+    return render_template("index.html")
+    
+@app.route("/game")
+def game():
+    return render_template("game.html")
+    
+@app.route("/leaderboard")
+def leaderboard():
+    return render_template("leaderboard.html")
 
 if __name__ == "__main__":
     app.run(host = os.getenv('IP'), port = int(os.getenv('PORT')), debug = True)
