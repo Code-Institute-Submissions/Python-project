106bca167e801ab035353ef009fac71dacfb9a8a
Geoff Doig <geoff.doig71@gmail.com>
2019-01-10 17:02:17 +0000

    Created sign in form and session variable for username

--- run.py
+++ run.py
@@ -1,18 +1,26 @@
 import os
-from flask import Flask, render_template
+from flask import Flask, render_template, request, session
 
 app = Flask(__name__)
+app.secret_key = "secret123string"
 
-@app.route("/")
+@app.route("/", methods = ["GET", "POST"])
 def index():
+    """Welcome / sign in page"""
+    if request.method == "POST":
+        session["username"] = request.form["username"]
+    if "username" in session:
+        return render_template("game.html")
     return render_template("index.html")
     
 @app.route("/game")
 def game():
+    """Main game page"""
     return render_template("game.html")
     
 @app.route("/leaderboard")
 def leaderboard():
+    """Show leaderboard page"""
     return render_template("leaderboard.html")
 
 if __name__ == "__main__":
