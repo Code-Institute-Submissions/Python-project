fca1a54ab36f39f69a4da64f97dc23d5672bb331
Geoff Doig <geoff.doig71@gmail.com>
2019-01-23 18:05:38 +0000

    Changed debug for deployment

--- run.py
+++ run.py
@@ -101,4 +101,4 @@ def logout():
     return redirect(url_for("index"))
 
 if __name__ == "__main__":
-    app.run(host = os.getenv('IP', '0.0.0.0'), port = int(os.getenv('PORT', '5000')), debug = True)
+    app.run(host = os.getenv('IP', '0.0.0.0'), port = int(os.getenv('PORT', '5000')), debug = False)
